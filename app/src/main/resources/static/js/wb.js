c202953f8e3794aafd7f952efe97ff6c6be903e2
AndreiMaroiu <79592738+AndreiMaroiu@users.noreply.github.com>
2022-01-23 13:31:19 +0200

    added js for websockets dependencies

---
+++ app/src/main/resources/static/js/wb.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("Index page is ready");
+});
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     console.log("Hello! I am loaded!");
+// });
